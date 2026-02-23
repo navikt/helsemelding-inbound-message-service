4f763808fc5930b8117d7f4b77023bb87482b982
Marius Syverstad Fossum <marius.syverstad.fossum@nav.no>
2026-02-23 12:31:22 +0100

    Add poller for new messages (#3)

--- src/main/kotlin/no/nav/helsemelding/inbound/Dependencies.kt
+++ src/main/kotlin/no/nav/helsemelding/inbound/Dependencies.kt
@@ -6,11 +6,16 @@ import arrow.fx.coroutines.await.awaitAll
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.micrometer.prometheus.PrometheusConfig.DEFAULT
 import io.micrometer.prometheus.PrometheusMeterRegistry
+import no.nav.helsemelding.ediadapter.client.EdiAdapterClient
+import no.nav.helsemelding.ediadapter.client.HttpEdiAdapterClient
+import no.nav.helsemelding.ediadapter.client.scopedAuthHttpClient
+import no.nav.helsemelding.inbound.config.EdiAdapter
 
 private val log = KotlinLogging.logger {}
 
 data class Dependencies(
-    val meterRegistry: PrometheusMeterRegistry
+    val meterRegistry: PrometheusMeterRegistry,
+    val ediAdapterClient: EdiAdapterClient
 )
 
 internal suspend fun ResourceScope.metricsRegistry(): PrometheusMeterRegistry =
@@ -18,10 +23,19 @@ internal suspend fun ResourceScope.metricsRegistry(): PrometheusMeterRegistry =
         p.close().also { log.info { "Closed prometheus registry" } }
     }
 
+internal suspend fun ResourceScope.ediAdapterClient(ediAdapter: EdiAdapter): EdiAdapterClient =
+    install({ HttpEdiAdapterClient(scopedAuthHttpClient(ediAdapter.scope.value)) }) { p, _: ExitCase ->
+        p.close().also { log.info { "Closed edi adapter client" } }
+    }
+
 suspend fun ResourceScope.dependencies(): Dependencies = awaitAll {
+    val config = config()
+
     val metricsRegistry = async { metricsRegistry() }
+    val ediAdapterClient = async { ediAdapterClient(config.ediAdapter) }
 
     Dependencies(
-        metricsRegistry.await()
+        metricsRegistry.await(),
+        ediAdapterClient.await()
     )
 }
