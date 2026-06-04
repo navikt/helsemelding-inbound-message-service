7758726dd4cec49ace37759d8a23cc5e31dd4c32
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2026-06-04 13:33:43 +0200

    Oppdatert attachment-client til v0.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,7 +68,7 @@ dependencies {
     implementation(libs.token.validation.ktor.v3)
     implementation(libs.kotlin.kafka)
     implementation(libs.edi.adapter.client)
-    implementation("no.nav.helsemelding:attachment-client:0.0.1-SNAPSHOT-3")
+    implementation("no.nav.helsemelding:attachment-client:0.0.1")
 
     testImplementation(testLibs.bundles.kotest)
     testImplementation(testLibs.kotest.assertions.arrow)
